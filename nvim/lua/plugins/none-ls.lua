1efc808b52b970273ea14e0224e0db557cc76eae
Sakshi Tomar <sakshiitomar89@gmail.com>
2025-03-02 12:41:43 +0530

    add golangci-lint

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,27 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-        -- null_ls.builtins.formatting.prettierd
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.diagnostics.golangci_lint.with({
+					command = "golangci-lint",
+					args = {
+						"run",
+						"--config=.golangci.yml",
+						"--out-format=json",
+					},
+					cwd = function(params)
+						return vim.fn.getcwd()
+					end,
+				}),
+				-- null_ls.builtins.formatting.prettierd
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
